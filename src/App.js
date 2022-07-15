223aa543e990f5441c584c3066c8e3eaa0a7bd4a
arulkumararul2000 <arulkumararul2000@gmail.com>
2022-07-15 15:28:18 +0530

    upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React,{useState } from "react";
+import './App.css';
+
+function App() {
+
+const [url,setUrl]=useState('');
+const [ip,setIp]=useState('');
+
+ 
+
+const print=()=>{
+  window.location.href=url+ip;
+}
+
+  return (
+  <>
+  <form className="container">
+    <div className="col-lg-12 col-md-12 col-sm-12">
+      <label>Host</label>
+      <select className="form-select col-lg-4 col-md-8 col-sm-8 form-control" value={url} onChange={(e)=>{setUrl(e.target.value)}}>
+        <option >select</option>
+        <option value="http://">http</option>
+      </select>
+      <label>IP Address</label>
+      <input className="form-input col-lg-4 col-md-8 col-sm-8 form-control" value={ip} onChange={(e)=>{setIp(e.target.value)}} />
+      <button className="form-btn col-lg-2 col-md-1 col-sm-1" onClick={print}>add</button>
+    </div>
+  </form>
+  </>
+  );
+}
+export default App;
